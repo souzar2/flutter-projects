e4fc993672371799dbaeab4186863ec2c8c23ac7
souzar2 <58992704+souzar2@users.noreply.github.com>
2020-07-16 16:51:14 -0300

    Update main.dart

--- Lista de Tarefas/main.dart
+++ Lista de Tarefas/main.dart
@@ -16,9 +16,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  @override
+
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPas;
+
+  @override
+
+  void initState(){
+    super.initState();
+    _readData().then((data){
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
   void _addToDo() {
     setState(() {
       Map<String, dynamic> newToDo = Map();
@@ -26,8 +40,31 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+      _saveData();
     });
   }
+
+  Future<Null> refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+   setState(() {
+     _toDoList.sort((a, b) {
+       if (a["ok"] && !b["ok"]) {
+         return 1;
+       }
+       else if (!a["ok"] && b["ok"]) {
+         return -1;
+       }
+       else {
+         return 0;
+       }
+     });
+
+     _saveData();
+
+   });
+   return null;
+  }
+
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,40 +99,81 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
+            child: RefreshIndicator(onRefresh: refresh,
             child: ListView.builder(
                 padding: EdgeInsets.only(top: 10),
                 itemCount: _toDoList.length,
-                itemBuilder: (context, index){
-                  return CheckboxListTile(
-                    title: Text(_toDoList[index]["title"]),
-                    value: _toDoList[index]["ok"],
-                    secondary: CircleAvatar(
-                      child: Icon(_toDoList[index]["ok"]?
-                      Icons.check : Icons.error,)
-                    ),
-                    onChanged: (c){
-                      setState(() {
-                        _toDoList[index]["ok"] = c;
-                      });
-                    },
-                  );
-                }),
+                itemBuilder: buildItem),)
           )
         ],
       )
     );
   }
 
+  Widget buildItem(context, index){
+  return Dismissible(
+    key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+    background: Container(
+      color: Colors.red,
+      child: Align(
+        alignment: Alignment(0.9,0.0),
+        child: Icon(Icons.delete, color: Colors.white,),
+      )
+    ),
+    direction: DismissDirection.startToEnd,
+    child: CheckboxListTile(
+      title: Text(_toDoList[index]["title"]),
+      value: _toDoList[index]["ok"],
+      secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"]?
+          Icons.check : Icons.error,)
+      ),
+      onChanged: (c){
+        setState(() {
+          _toDoList[index]["ok"] = c;
+          _saveData();
+        });
+      },
+    ),
+    onDismissed: (direction) {
+      setState(() {
+        _lastRemoved = Map.from(_toDoList[index]);
+        _lastRemovedPas = index;
+        _toDoList.removeAt(index);
+        _saveData();
+
+
+        final snack = SnackBar(
+          content: Text("\"${_lastRemoved["title"]}\" removed"),
+          action: SnackBarAction(label: "Desfazer",
+          onPressed: (){
+            setState(() {
+              _toDoList.insert(_lastRemovedPas, _lastRemoved);
+              _saveData();
+            });
+          },
+          ),
+          duration: Duration(seconds: 2),
+        );
+
+        Scaffold.of(context).removeCurrentSnackBar();
+        Scaffold.of(context).showSnackBar(snack);
+      });
+
+    },
+  );
+  }
+
   Future<File> _getFile() async{
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.path");
   }
-  Future<File> _saveFile() async {
+  Future<File> _saveData() async {
     String data = json.encode(_toDoList);
     final File = await _getFile();
     return File.writeAsString(data);
   }
-  Future<String> _getData() async{
+  Future<String> _readData() async{
     try{
       final file = await _getFile();
           return file.readAsString();
